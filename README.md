7fb7e797415f3ff16c2cd555fc79fd77256aa195
TJiay <13576712617@163.com>
2021-05-08 20:51:12 +0800

    Format & Add one feature

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 **Note: the python verison is 3.8+**
 **run `python main.py` to start practice writing poems on silent**
 
+## Features
+1. You can customize your own poem date
+
 ## Some problems to be improved
 - [x] Verify the answer.
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from os import system
+from os import system, listdir
 from re import findall
 from random import randint, random
 from sys import version as python_version
@@ -19,41 +19,59 @@ class Poems(object):
                 self.chooseNumber = randint(0, self.poemsLens[i] - 1)
                 self.poemsLenArray = [len(k) for k in self.poemsArray[i]]
                 for j in range(self.poemsLens[i]):
-                    if j == self.chooseNumber: print('\033[1m', self.poemsArray[i][j], '\033[0m', end='')
-                    if j < self.chooseNumber: print(underline(self.poemsLenArray[j]), end='，')
-                    if j > self.chooseNumber: print(',', underline(self.poemsLenArray[j]), end='')
+                    if j == self.chooseNumber:
+                        print('\033[1m', self.poemsArray[i][j], '\033[0m', end='')
+                    if j < self.chooseNumber:
+                        print(underline(self.poemsLenArray[j]), end='，')
+                    if j > self.chooseNumber:
+                        print(',', underline(self.poemsLenArray[j]), end='')
                 self.Answer(i)
-                self.Tonext()
+                self.Next()
 
     def Answer(self, n: int):
-        def f(i: int): return i + 1 if i + 1 > self.chooseNumber else i
+        def f(i: int):
+            # return i + 1 > self.chooseNumber ? i + 1 : i
+            return i + 1 if i + 1 > self.chooseNumber else i 
         for i in range(self.poemsLens[n] - 1):
             answers_input = input(f"\n Your answers for {i} ['a' to see the answers]: ")
-            if answers_input == 'a': print('\n The Answers: ', self.poemsArray[n][f(i)])
-            elif answers_input == self.poemsArray[n][f(i)]: print('\033[1;32m', 'Good job!', '\033[0m')
+            if answers_input == 'a': # print answers
+                print('\n The Answers: ', self.poemsArray[n][f(i)])
+            elif answers_input == self.poemsArray[n][f(i)]: 
+                print('\033[1;32m', 'Good job!', '\033[0m')
             else:
                 print('\033[1;31m', 'Error!', '\033[0m')
                 print(' The Answers: ', self.poemsArray[n][f(i)])
 
-    def Tonext(self):
-        continue_or_exit = input("\n Do you want to continue or exit?['q' to exit]: ")
-        if continue_or_exit == 'q': print('\033[1;32m', 'Goodbye!', '\033[0m'); exit(0)
+    def Next(self):
+        next_or_exit = input("\n Do you want to continue or exit?['q' to exit]: ")
+        if next_or_exit == 'q': 
+            print('\033[1;32m', 'Goodbye!', '\033[0m')
+            exit(0)
         system('clear')
 
 system('clear')
 ob = Poems('')
-count = 0
-Grade = input(" Chooes grade[7, 8, 9]: ")
-DatePath = f"./Data/PoemsOfGrade{Grade}.txt"
-
+count, grade_arr = 0, []
 max_number_of_poems = 50
+
+# Read Date list
+listDir = listdir("./Data/")
+for filename in listDir:
+    # print(filename)
+    grade_arr.append(int(findall(r"PoemsOfGrade(.*).txt", filename)[0]))
+
+# Begin
+Grade = input(f" Chooes grade{grade_arr}: ")
+
 number_of_poems = int(input("\n Input number of poem[Max: 50]: "))
 while(number_of_poems >= max_number_of_poems):
     print('\033[1;31m', 'Warning: the number is bigger than 50', '\033[0m')
     number_of_poems = int(input("\n Input number of poem[Max: 50]: "))
 
+DatePath = f"./Data/PoemsOfGrade{Grade}.txt"
+
 with open(DatePath, 'r') as poems_file:
-    if int(python_version[2]) < 8:
+    if int(python_version[2]) < 8: # python_version < 3.8
         i = 0
         while i and (count != number_of_poems):
             if i[0] == '#': continue
@@ -61,12 +79,15 @@ with open(DatePath, 'r') as poems_file:
             count = count + 1
             i = poems_file.readline()
             ob.Println(i.rstrip("\n"))
-    else:
+    else: # python_version >= 3.8
         while (i := poems_file.readline()) and (count != number_of_poems):
             if i[0] == '#': continue
             system('clear')
             count = count + 1
             ob.Println(i.rstrip("\n"))
 
-if number_of_poems == 0: print('\033[1;31m', 'What are you doing?', '\033[0m')
-else: print('\033[1;32m', 'You are great!You passed!😎', '\033[0m')
+# End
+if number_of_poems == 0:
+    print('\033[1;31m', 'What are you doing?', '\033[0m')
+else:
+    print('\033[1;32m', 'You are great!You passed!😎', '\033[0m')
